c9556c855e306779988ada1b8d8e9018244e2f95
OSINSKI <S6284463@live.tees.ac.uk>
2018-10-02 13:18:06 +0100

    Add project files.

---
+++ Week 1 Apps/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Week_1_Apps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
